b0cf4b145446bbbc458f3ab322ed53500c19982e
Dhruva Singh <singhdhruva45@gmail.com>
2024-12-20 15:34:49 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
             <ModalProvider />
             <Sidebar>
               {children}
-              <Analytics/>
+              <Analytics />
             </Sidebar>
           </UserProvider>
         </SupabaseProvider>
